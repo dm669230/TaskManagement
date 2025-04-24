c8bc2b28de33e6873d23251794c6acb8310ac661
Vaibhav <viabhav.shrivastava@factsonline.in>
2025-04-25 02:51:10 +0530

    backend3 dir removed

--- backend3/tasks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Task(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    effort_to_complete = models.IntegerField()
-    due_date = models.DateField()
-    user_id = models.IntegerField()
